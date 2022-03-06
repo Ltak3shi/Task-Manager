1e638924470754c4c910c981e1b984a239ff8b9e
Ltak3shi <lucasthirata2011@hotmail.com>
2022-03-05 22:11:01 -0300

    added some error handling

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,10 +1,11 @@
 const Task = require('../models/task')
-const statusCodes = require('http-status-codes')
+const {StatusCodes} = require('http-status-codes')
+const CustomAPIError = require('../errors/custom-error')
 
 const getAllTasks = async (req, res, next) => {
     try {
         const tasks = await Task.find({})
-        res.status(statusCodes.OK).json({tasks})
+        res.status(StatusCodes.OK).json({tasks})
     } catch (error) {
         console.log(error)
     }
@@ -13,35 +14,47 @@ const getTask = async (req, res, next) => {
     const {id} = req.params
     try {
         const task = await Task.findOne({_id: id})
-        res.status(statusCodes.OK).json({task})
+        if(!task){
+            throw new CustomAPIError(`id: ${id} does not exist`, StatusCodes.NOT_FOUND)
+        }
+        res.status(StatusCodes.OK).json({task})
     } catch (error) {
-        console.log(error)
+        next(error)
     }
 }
 const createTask = async (req, res, next) => {
     try {
+        if(!req.body.name){
+            throw new CustomAPIError(`Please provide a name`, StatusCodes.BAD_REQUEST)
+        }
         const task = await Task.create(req.body)
-        res.status(statusCodes.CREATED).json({task})
+        res.status(StatusCodes.CREATED).json({task})
     } catch (error) {
-        console.log(error)
+        next(error)
     }
 }
 const updateTask = async (req, res, next) => {
     const {id} = req.params
     try {
         const task = await Task.findOneAndUpdate({_id: id}, req.body, {new:true})
-        res.status(statusCodes.OK).json({task})
+        if(!task){
+            throw new CustomAPIError(`id: ${id} does not exist`, StatusCodes.NOT_FOUND)
+        }
+        res.status(StatusCodes.OK).json({task})
     } catch (error) {
-        console.log(error)
+        next(error)
     }
 }
 const deleteTask = async (req, res, next) => {
     const {id} = req.params
     try {
         const task = await Task.findOneAndDelete({_id: id})
-        res.status(statusCodes.OK).send(`Task: ${task.name} has been deleted`)
+        if(!task){
+            throw new CustomAPIError(`id: ${id} does not exist`, StatusCodes.NOT_FOUND)
+        }
+        res.status(StatusCodes.OK).send(`Task: ${task.name} has been deleted`)
     } catch (error) {
-        console.log(error)
+        next(error)
     }
 }
 
